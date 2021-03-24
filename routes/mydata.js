9566e4bd862505e468a4e27ae56ec3740ca8aa71
prayojitha <prayojitha.bere@gmail.com>
2021-03-23 23:55:25 -0500

    rollback

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* mydata page */
+/* Getting into home page*/
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Keerthi Prayojitha Bere' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
