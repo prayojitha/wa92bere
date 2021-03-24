dccfa470a5a19ef34387513151f97bf7017c6eaf
prayojitha <prayojitha.bere@gmail.com>
2021-03-23 19:40:12 -0500

    new endpoint

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
 
 // catch 404 and forward to error handler
