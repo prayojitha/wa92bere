35ff90c913c904a5f944b1fbc72dda37d0d92978
prayojitha <prayojitha.bere@gmail.com>
2021-03-24 20:48:09 -0500

    Incrementing Users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var users = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  users = users + 1;
+  res.send('Users accesses are:' + users);
 });
 
 module.exports = router;
